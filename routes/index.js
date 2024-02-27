0894b1fbe2487eceb6809301eba99328874598c1
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2024-02-26 16:51:22 -0800

    save data to file

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
 
 // start by creating data so we don't have to type it in each time
 let serverArray = [];
@@ -18,10 +19,44 @@ let MovieObject = function (pTitle, pYear, pGenre, pMan, pWoman, pURL) {
 }
 
 
+
+
+let fileManager  = {
+  read: function() {
+    var rawdata = fs.readFileSync('objectdata.json');
+    let goodData = JSON.parse(rawdata);
+    serverArray = goodData;
+  },
+
+  write: function() {
+    let data = JSON.stringify(serverArray);
+    fs.writeFileSync('objectdata.json', data);
+  },
+
+  validData: function() {
+    var rawdata = fs.readFileSync('objectdata.json');
+    console.log(rawdata.length);
+    if(rawdata.length < 1) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+};
+  
+
+
+if(!fileManager.validData()) {
 serverArray.push(new MovieObject("Moonstruck", 1981, "Drama", "Nicholas Cage", "Cher", "https://www.youtube.com/watch?v=M01_2CKL6PU"));
 serverArray.push(new MovieObject("Wild At Heart", 1982, "Drama", "Nicholas Cage", "Laura VanDern", "https://www.youtube.com/watch?v=7uRJartX79Q"));
 serverArray.push(new MovieObject("Raising Arizona", 1983, "Comedy", "Nicholas Cage", "Holly Hunter", "https://www.youtube.com/watch?v=NoXJKArYi1g"));
 serverArray.push(new MovieObject("USS Indianapolis: Men of Courage", 2016, "Drama", "Nicholas Cage", "Emily Tennant", "https://youtu.be/ZDPE-NronKk"));
+fileManager.write();
+}
+else {
+  fileManager.read(); // do have prior movies so load up the array
+}
 
 console.log(serverArray);
 
@@ -33,6 +68,8 @@ router.get('/', function(req, res, next) {
 
 /* GET all Movie data */
 router.get('/getAllMovies', function(req, res) {
+  fileManager.read();
+  
   res.status(200).json(serverArray);
 });
 
@@ -40,6 +77,7 @@ router.get('/getAllMovies', function(req, res) {
 router.post('/AddMovie', function(req, res) {
   const newMovie = req.body;
   serverArray.push(newMovie);
+  fileManager.write();
   res.status(200).json(newMovie);
 });
 
@@ -57,6 +95,7 @@ router.delete('/DeleteMovie/:ID', (req, res) => {
   }
 else {    // if did find the movie
     serverArray.splice(pointer, 1);  // remove 1 element at index 
+    fileManager.write();
     res.send('Movie with ID: ' + delID + ' deleted!');
 }
 });
